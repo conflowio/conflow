b601f1e095e3e9dbafc1ad2843f16a1634861dd8
bandesz <bandesz@gmail.com>
2018-10-15 11:55:13 +0100

    Add Variable parser

---
+++ parser/variable.go
@@ -0,0 +1,120 @@
+// Copyright (c) 2017 Opsidian Ltd.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package parser
+
+import (
+	"fmt"
+
+	"github.com/opsidian/ocl/variable"
+	"github.com/opsidian/parsley/ast"
+	"github.com/opsidian/parsley/ast/interpreter"
+	"github.com/opsidian/parsley/combinator"
+	"github.com/opsidian/parsley/parsley"
+	"github.com/opsidian/parsley/text"
+	"github.com/opsidian/parsley/text/terminal"
+)
+
+// Variable will match a variable expression defined by the following rule, where P is the input parser:
+//   S         -> ID (VAR_INDEX)*
+//   ID        -> /[a-z][a-z0-9]*(?:_[a-z0-9]+)*/
+//   VAR_INDEX -> "." ID
+//             -> "[" P "]"
+func Variable(p parsley.Parser) *combinator.Recursive {
+	variableIndex := combinator.Seq(
+		terminal.Rune('.'),
+		ID(),
+	).Bind(interpreter.Select(1))
+	arrayIndex := combinator.Seq(
+		terminal.Rune('['),
+		text.LeftTrim(p, text.WsSpaces),
+		text.LeftTrim(terminal.Rune(']'), text.WsSpaces),
+	).Bind(interpreter.Select(1))
+
+	lookup := func(i int) parsley.Parser {
+		if i == 0 {
+			return ID()
+		}
+		return combinator.Choice(arrayIndex, variableIndex)
+	}
+	lenCheck := func(len int) bool {
+		return len > 0
+	}
+	return combinator.NewRecursive("VAR", lookup, lenCheck).Bind(ast.InterpreterFunc(evalVariable))
+}
+
+func evalVariable(ctx interface{}, nodes []parsley.Node) (interface{}, parsley.Error) {
+	value0, _ := nodes[0].Value(ctx)
+	name := value0.(string)
+	varIndex := make([]interface{}, 0, len(nodes)-1)
+	for i := 1; i < len(nodes); i++ {
+		val, err := nodes[i].Value(ctx)
+		if err != nil {
+			return nil, err
+		}
+		varIndex = append(varIndex, val)
+	}
+
+	variableProvider := ctx.(variable.Provider)
+
+	res, err := variableProvider.LookupVar(lookup(name, varIndex, nodes))
+	if err != nil {
+		varName := name
+		for _, index := range varIndex {
+			varName = varName + "[" + fmt.Sprintf("%v", index) + "]"
+		}
+		if err == variable.ErrNotFound {
+			return nil, parsley.WrapError(
+				parsley.NewError(nodes[0].Pos(), err),
+				"variable '%s' does not exist", varName,
+			)
+		}
+
+		return nil, parsley.NewError(nodes[0].Pos(), err)
+	}
+	return res, nil
+}
+
+func lookup(name string, varIndex []interface{}, nodes []parsley.Node) variable.LookUp {
+	return func(provider variable.Provider) (interface{}, error) {
+		res, ok := provider.GetVar(name)
+		if !ok {
+			return nil, variable.ErrNotFound
+		}
+		for i, index := range varIndex {
+			switch rest := res.(type) {
+			case []interface{}:
+				switch indext := index.(type) {
+				case int64:
+					if indext >= 0 && indext < int64(len(rest)) {
+						res = rest[indext]
+					} else {
+						indexNode := nodes[i+1].(*ast.NonTerminalNode).Children()[1]
+						return nil, parsley.NewErrorf(indexNode.Pos(), "array index out of bounds: %d (0..%d)", indext, len(rest)-1)
+					}
+				default:
+					indexNode := nodes[i+1].(*ast.NonTerminalNode).Children()[1]
+					return nil, parsley.NewErrorf(indexNode.Pos(), "invalid non-integer index on array")
+				}
+			case map[string]interface{}:
+				switch indext := index.(type) {
+				case string:
+					res, ok = rest[indext]
+					if !ok {
+						return nil, variable.ErrNotFound
+					}
+				default:
+					indexNode := nodes[i+1].(*ast.NonTerminalNode).Children()[1]
+					return nil, parsley.NewErrorf(indexNode.Pos(), "invalid non-string index on map")
+				}
+			default:
+				indexNode := nodes[i+1].(*ast.NonTerminalNode).Children()[1]
+				return nil, parsley.NewErrorf(indexNode.Pos(), "can not get index on %s type", fmt.Sprintf("%T", res))
+			}
+		}
+		return res, nil
+	}
+}
---
+++ parser/variable_test.go
@@ -0,0 +1,71 @@
+package parser_test
+
+import (
+	"errors"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	"github.com/opsidian/ocl/parser"
+	"github.com/opsidian/ocl/test"
+	"github.com/opsidian/parsley/combinator"
+	"github.com/opsidian/parsley/text/terminal"
+)
+
+var _ = Describe("Variable", func() {
+
+	q := combinator.Choice(
+		terminal.String(false),
+		terminal.Integer(),
+		terminal.Word("nil", nil),
+		test.EvalErrorParser(),
+	).ReturnError("was expecting value")
+
+	p := parser.Variable(q)
+
+	DescribeTable("it evaluates the input correctly",
+		func(input string, expected interface{}) {
+			test.ExpectParserToEvaluate(p)(input, expected)
+		},
+		test.TableEntry(`foo`, "bar"),
+		test.TableEntry(`map.key1`, "value1"),
+		test.TableEntry(`map["key1"]`, "value1"),
+		test.TableEntry(`map.key2.key3`, "value3"),
+		test.TableEntry(`map["key2"].key3`, "value3"),
+		test.TableEntry(`map.key2["key3"]`, "value3"),
+		test.TableEntry(`map.key4[0]`, "value4"),
+		test.TableEntry(`arr[0]`, "value1"),
+		test.TableEntry(`arr[1][0]`, "value2"),
+		test.TableEntry(`arr[2].key1`, "value3"),
+		test.TableEntry(`arr[2]["key1"]`, "value3"),
+	)
+
+	DescribeTable("it returns a parse error",
+		func(input string, expectedErr error) {
+			test.ExpectParserToHaveParseError(p)(input, expectedErr)
+		},
+		test.TableEntry(`map.`, errors.New("was expecting the end of input at testfile:1:4")),
+		test.TableEntry(`map.key1.`, errors.New("was expecting the end of input at testfile:1:9")),
+		test.TableEntry(`map[`, errors.New("was expecting the end of input at testfile:1:4")),
+		test.TableEntry(`map["key1"`, errors.New("was expecting the end of input at testfile:1:4")),
+		test.TableEntry(`map[]`, errors.New("was expecting the end of input at testfile:1:4")),
+		test.TableEntry(`arr.0`, errors.New("was expecting the end of input at testfile:1:4")),
+		test.TableEntry(`arr[1].0`, errors.New("was expecting the end of input at testfile:1:7")),
+	)
+
+	DescribeTable("it returns an eval error",
+		func(input string, expectedErr error) {
+			test.ExpectParserToHaveEvalError(p)(input, expectedErr)
+		},
+		test.TableEntry(`nonexisting`, errors.New("variable 'nonexisting' does not exist at testfile:1:1")),
+		test.TableEntry(`nonexisting.key`, errors.New("variable 'nonexisting[key]' does not exist at testfile:1:1")),
+		test.TableEntry(`map.nonexisting`, errors.New("variable 'map[nonexisting]' does not exist at testfile:1:1")),
+		test.TableEntry(`arr[3]`, errors.New("array index out of bounds: 3 (0..2) at testfile:1:5")),
+		test.TableEntry(`arr[-1]`, errors.New("array index out of bounds: -1 (0..2) at testfile:1:5")),
+		test.TableEntry(`map.key1.key2`, errors.New("can not get index on string type at testfile:1:10")),
+		test.TableEntry(`map.key2[0]`, errors.New("invalid non-string index on map at testfile:1:10")),
+		test.TableEntry(`arr["string"]`, errors.New("invalid non-integer index on array at testfile:1:5")),
+		test.TableEntry(`arr[ERR]`, errors.New("ERR at testfile:1:5")),
+		test.TableEntry(`map[ERR]`, errors.New("ERR at testfile:1:5")),
+	)
+
+})
