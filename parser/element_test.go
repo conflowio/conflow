4573a10f2efcb0c1dec60a14d27c248d1c23c796
bandesz <bandesz@gmail.com>
2018-10-15 17:05:34 +0100

    Provide better error messages than 'was expecting the end of input' by using the context error (Parsley 0.10.1) change

--- parser/element_test.go
+++ parser/element_test.go
@@ -47,11 +47,11 @@ var _ = Describe("Element", func() {
 		func(input string, expectedErr error) {
 			test.ExpectParserToHaveParseError(p)(input, expectedErr)
 		},
-		test.TableEntry(`MAP.`, errors.New("was expecting the end of input at testfile:1:4")),
-		test.TableEntry(`MAP.key1.`, errors.New("was expecting the end of input at testfile:1:9")),
-		test.TableEntry(`MAP[`, errors.New("was expecting the end of input at testfile:1:4")),
-		test.TableEntry(`MAP["key1"`, errors.New("was expecting the end of input at testfile:1:4")),
-		test.TableEntry(`MAP[]`, errors.New("was expecting the end of input at testfile:1:4")),
+		test.TableEntry(`MAP.`, errors.New("was expecting ID at testfile:1:5")),
+		test.TableEntry(`MAP.key1.`, errors.New("was expecting ID at testfile:1:10")),
+		test.TableEntry(`MAP[`, errors.New("was expecting value at testfile:1:5")),
+		test.TableEntry(`MAP["key1"`, errors.New("was expecting \"]\" at testfile:1:11")),
+		test.TableEntry(`MAP[]`, errors.New("was expecting value at testfile:1:5")),
 	)
 
 	DescribeTable("it returns an eval error",
