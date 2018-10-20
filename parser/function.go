264c7cedf830a178fba701f6c71cc07ed6e86eec
bandesz <bandesz@gmail.com>
2018-10-20 11:46:08 +0100

    Add function parser

---
+++ parser/function.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2017 Opsidian Ltd.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package parser
+
+import (
+	"errors"
+
+	"github.com/opsidian/ocl/function"
+	"github.com/opsidian/parsley/ast"
+	"github.com/opsidian/parsley/combinator"
+	"github.com/opsidian/parsley/parsley"
+	"github.com/opsidian/parsley/text"
+	"github.com/opsidian/parsley/text/terminal"
+)
+
+// Function will match a function call defined by the following rule, where P is the input parser:
+//   S      -> ID "(" PARAMS ")"
+//   ID     -> /[a-z][a-z0-9]*(?:_[a-z0-9]+)*/
+//   PARAMS -> EMPTY
+//          -> P ("," P)*
+func Function(p parsley.Parser) *combinator.Recursive {
+	return combinator.Seq(
+		ID(),
+		terminal.Rune('('),
+		text.LeftTrim(SepByComma(p, text.WsSpaces), text.WsSpaces),
+		text.LeftTrim(terminal.Rune(')'), text.WsSpaces),
+	).Bind(ast.InterpreterFunc(evalFunction))
+}
+
+func evalFunction(ctx interface{}, nodes []parsley.Node) (interface{}, parsley.Error) {
+	registry := ctx.(function.Registry)
+
+	functioNode := nodes[0]
+	name, _ := functioNode.Value(ctx)
+
+	if !registry.FunctionExists(name.(string)) {
+		return nil, parsley.NewError(functioNode.Pos(), errors.New("function does not exist"))
+	}
+
+	paramsNode := nodes[2].(*ast.NonTerminalNode)
+	var params []parsley.Node
+	children := paramsNode.Children()
+	childrenCount := len(children)
+	if childrenCount > 0 {
+		params = make([]parsley.Node, childrenCount/2+1)
+		for i := 0; i < childrenCount; i += 2 {
+			params[i/2] = children[i]
+		}
+	}
+	return registry.CallFunction(ctx, nodes[0], params)
+}
---
+++ parser/function_test.go
@@ -0,0 +1,54 @@
+package parser_test
+
+import (
+	"errors"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	"github.com/opsidian/ocl/parser"
+	"github.com/opsidian/ocl/test"
+	"github.com/opsidian/parsley/combinator"
+	"github.com/opsidian/parsley/text/terminal"
+)
+
+var _ = Describe("Function", func() {
+
+	var p *combinator.Recursive
+
+	q := combinator.Choice(
+		terminal.String(false),
+		terminal.Word("nil", nil),
+		parser.Variable(p),
+		test.EvalErrorParser(),
+	).ReturnError("was expecting value")
+
+	p = parser.Function(q)
+
+	DescribeTable("it evaluates the input correctly",
+		func(input string, expected interface{}) {
+			test.ExpectParserToEvaluate(p)(input, expected)
+		},
+		test.TableEntry(`rand()`, int64(123)),
+		test.TableEntry(`upper("foo")`, "FOO"),
+		test.TableEntry(`default(nil, "default")`, "default"),
+	)
+
+	DescribeTable("it returns a parse error",
+		func(input string, expectedErr error) {
+			test.ExpectParserToHaveParseError(p)(input, expectedErr)
+		},
+		test.TableEntry("upper", errors.New("was expecting \"(\" at testfile:1:6")),
+		test.TableEntry("upper(", errors.New("was expecting \")\" at testfile:1:7")),
+		test.TableEntry(`upper("foo"`, errors.New("was expecting \")\" at testfile:1:12")),
+		test.TableEntry(`upper("foo",`, errors.New("was expecting value at testfile:1:13")),
+	)
+
+	DescribeTable("it returns an eval error",
+		func(input string, expectedErr error) {
+			test.ExpectParserToHaveEvalError(p)(input, expectedErr)
+		},
+		test.TableEntry(`nonexisting("foo")`, errors.New("function does not exist at testfile:1:1")),
+		test.TableEntry("upper(ERR)", errors.New("ERR at testfile:1:7")),
+	)
+
+})
