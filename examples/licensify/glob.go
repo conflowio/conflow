bb7859e373b052c6130622dcf7db8e9f881a435b
bandesz <bandesz@gmail.com>
2019-06-07 12:47:49 +0100

    Add licensify example

---
+++ examples/licensify/glob.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2017 Opsidian Ltd.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/opsidian/basil/basil/block"
+
+	"github.com/opsidian/basil/basil"
+)
+
+//go:generate basil generate
+type Glob struct {
+	id      basil.ID                `basil:"id"`
+	path    string                  `basil:"required"`
+	pattern string                  `basil:"required"`
+	file    chan basil.BlockMessage `basil:"block,output"`
+}
+
+func (g *Glob) Main(ctx basil.BlockContext) error {
+	regexp, err := regexp.Compile(g.pattern)
+	if err != nil {
+		return err
+	}
+	return filepath.Walk(g.path, func(path string, info os.FileInfo, err error) error {
+		if !regexp.MatchString(path) {
+			return nil
+		}
+		message := basil.NewBlockMessage(&File{path: path})
+		select {
+		case g.file <- message:
+			<-message.WaitGroup().Wait()
+		case <-ctx.Context().Done():
+			return errors.New("aborted")
+		}
+		return nil
+	})
+}
+
+func (g *Glob) ParseContextOverride() basil.ParseContextOverride {
+	return basil.ParseContextOverride{
+		BlockTransformerRegistry: block.InterpreterRegistry{
+			"file": FileInterpreter{},
+		},
+	}
+}
+
+//go:generate basil generate
+type File struct {
+	id   basil.ID `basil:"id"`
+	path string
+}
---
+++ examples/licensify/licensify.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2017 Opsidian Ltd.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/opsidian/basil/basil"
+)
+
+//go:generate basil generate
+type Licensify struct {
+	id      basil.ID `basil:"id"`
+	path    string   `basil:"required"`
+	license string   `basil:"required"`
+}
+
+func (l *Licensify) Main(ctx basil.BlockContext) error {
+	content, err := ioutil.ReadFile(l.path)
+	if err != nil {
+		return err
+	}
+
+	if bytes.HasPrefix(content, []byte("// Code generated")) {
+		return nil
+	}
+
+	if bytes.Compare(content[0:len(l.license)], []byte(l.license)) != 0 {
+		buf := bytes.NewBufferString(l.license)
+		buf.Write(content)
+		if err := ioutil.WriteFile(l.path, buf.Bytes(), 0644); err != nil {
+			return err
+		}
+		fmt.Printf("%s was updated\n", l.path)
+	}
+
+	return nil
+}
