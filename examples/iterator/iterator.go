ab0ef36c2283b4eb4af6da549b56e320583d5f0c
bandesz <bandesz@gmail.com>
2019-06-08 16:56:36 +0100

    Add iterator example

---
+++ examples/iterator/iterator.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2017 Opsidian Ltd.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	"errors"
+
+	"github.com/opsidian/basil/basil/block"
+
+	"github.com/opsidian/basil/basil"
+)
+
+//go:generate basil generate
+type Iterator struct {
+	id    basil.ID                `basil:"id"`
+	count int64                   `basil:"required"`
+	it    chan basil.BlockMessage `basil:"output,block"`
+}
+
+func (it *Iterator) Main(ctx basil.BlockContext) error {
+	for i := int64(0); i < it.count; i++ {
+		message := basil.NewBlockMessage(&It{value: i})
+		it.it <- message
+		ctx.Logger().Debug().ID("id", it.id).Int64("i", i).Msg("ITERATOR WAITING")
+		select {
+		case <-message.WaitGroup().Wait():
+		case <-ctx.Context().Done():
+			return errors.New("aborted")
+		}
+		ctx.Logger().Debug().ID("id", it.id).Int64("i", i).Msg("ITERATOR FINISHED")
+	}
+
+	return nil
+}
+
+func (it *Iterator) ParseContextOverride() basil.ParseContextOverride {
+	return basil.ParseContextOverride{
+		BlockTransformerRegistry: block.InterpreterRegistry{
+			"it": ItInterpreter{},
+		},
+	}
+}
+
+//go:generate basil generate
+type It struct {
+	id    basil.ID `basil:"id"`
+	value int64    `basil:"output"`
+}
---
+++ examples/iterator/main.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2017 Opsidian Ltd.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	"github.com/opsidian/basil/function"
+
+	"github.com/opsidian/basil/examples/common"
+	"github.com/opsidian/basil/parser"
+	"github.com/opsidian/basil/util"
+
+	"github.com/opsidian/basil/basil"
+	"github.com/opsidian/basil/basil/block"
+)
+
+//go:generate basil generate
+type Main struct {
+	id basil.ID `basil:"id"`
+}
+
+func (m *Main) ParseContextOverride() basil.ParseContextOverride {
+	return basil.ParseContextOverride{
+		BlockTransformerRegistry: block.InterpreterRegistry{
+			"iterator": IteratorInterpreter{},
+			"print":    common.PrintInterpreter{},
+			"println":  common.PrintlnInterpreter{},
+			"do":       MainInterpreter{},
+		},
+		FunctionTransformerRegistry: function.Registry(),
+	}
+}
+
+func main() {
+	ctx, cancel := util.CreateDefaultContext()
+	defer cancel()
+
+	parseCtx := basil.NewParseContext(basil.NewIDRegistry(8, 16))
+
+	p := parser.NewMain("main", MainInterpreter{})
+
+	if err := p.ParseFile(
+		parseCtx,
+		"/Users/bandesz/go-workspace/src/github.com/opsidian/basil/examples/iterator/main.basil",
+	); err != nil {
+		panic(err)
+	}
+
+	common.Main(ctx, parseCtx)
+}
