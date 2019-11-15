d150aabae17c1d98da7df12bffcad81699e3ceb9
bandesz <bandesz@gmail.com>
2019-11-15 17:00:42 +0000

    Shellcheck fix in test.sh: add dquotes for dir

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ echo -n "" > coverage.txt
 
 export GO111MODULE=on
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -v -race -coverprofile=profile.out -covermode=atomic $d
+for dir in $(go list ./... | grep -v vendor); do
+    go test -v -race -coverprofile=profile.out -covermode=atomic "$dir"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
