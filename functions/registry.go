0dffe38a88a3883c13c4e6675dcfa26b401ee461
bandesz <bandesz@gmail.com>
2021-11-13 17:25:12 +0000

    Add format() function to implement fmt.Sprintf()

--- functions/registry.go
+++ functions/registry.go
@@ -16,6 +16,7 @@ import (
 func DefaultRegistry() function.InterpreterRegistry {
 	return function.InterpreterRegistry{
 		"array_contains":          ArrayContainsInterpreter{},
+		"format":                  FormatInterpreter{},
 		"len":                     LenInterpreter{},
 		"string":                  StringInterpreter{},
 		"math.abs":                math.AbsInterpreter{},
